b6a8c35e023939b92f6232c6be7cb0d1414eab4e
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-07-11 13:35:46 +0530

    InvertedPyramid Pattern

---
+++ JAVA_program_practice/src/patterns/InvertedPyramid.java
@@ -0,0 +1,27 @@
+package patterns;
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            //print spaces 
+            for(int j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=i;k<5;k++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+}
+}
+
+/* Output -->>
+ * * * * * 
+ * * * *  
+  * * * 
+   * *  
+    * 
+*/
